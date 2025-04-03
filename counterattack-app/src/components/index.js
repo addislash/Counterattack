559a7803ec8b3ce212d992eb4476540aca2bf4a4
addislash <63414733+addislash@users.noreply.github.com>
2025-04-03 15:29:55 -0500

    Delete counterattack-app directory

--- counterattack-app/src/components/index.js
+++
@@ -1,6 +0,0 @@
-// Export all components
-export { default as Logo } from './Logo';
-export { default as EmptyState } from './EmptyState';
-export { default as TournamentCard } from './TournamentCard';
-export { default as LoadingSpinner } from './LoadingSpinner';
-export { default as FileUploader } from './FileUploader';
